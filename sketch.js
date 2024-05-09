02c93823206ad8d53584a8a56f113f92f0f89b57
Tenzin2025 <115658881+Tenzin2025@users.noreply.github.com>
2024-05-09 12:07:13 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -48,18 +48,28 @@ function updateBlock() {
 function drawBlocks() {
   fill(250, 0, 0)
   rect(currentBlock.x, currentBlock.y, currentBlock.z, blockHeight);
+  fill(50);
   for(let block of placedBlocks) {
     rect(block.x, block.y, block.z, blockHeight);
   }
 }
 
 function placeBlock() {
+  const prevBlock= placedBlocks[placedBlocks.length-1];
+  let newWidth = blockWidth; 
+  
+  if(prevBlock) {
+  const leftEdge = max(prevBlock.x, currentBlock.x);
+  const rightEdge = min(prevBlock.x + prevBlock.z, currentBlock.x + currentBlock.z);
+  
+  newWidth = rightEdge - leftEdge;
+  }
+  
   placedBlocks.push(currentBlock);
   
-  newBlock();
+  newBlock(newWidth);
 }
 
-function newBlock() {
+function newBlock(newWidth) {
   const blockStackHeight= (placedBlocks.length + 1) * blockHeight;
-  currentBlock = createVector(0, height - blockStackHeight, blockWidth);
-}
+  currentBlock = createVector(0, height - blockStackHeight, newWidth);
